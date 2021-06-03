f21677dc1335dc11b835b9f596f4dd4e37ebb196
Ahmad Jailani <ahmadjailani604@gmail.com>
2021-06-03 11:02:27 +0700

    Move out App.kt from di folder

---
+++ app/src/main/java/com/ajailani/spesipon/App.kt
@@ -0,0 +1,7 @@
+package com.ajailani.spesipon
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application()
--- app/src/main/java/com/ajailani/spesipon/di/App.kt
+++
@@ -1,7 +0,0 @@
-package com.ajailani.spesipon.di
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class App : Application()
